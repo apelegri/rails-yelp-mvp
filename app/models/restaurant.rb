d24cb5fb09b971cc4aec75894568f4e3c10a1067
adrien pelegri <pelegriadrien@gmail.com>
2016-08-10 12:37:48 +0200

    Add restaurant model and setup front

---
+++ app/models/restaurant.rb
@@ -0,0 +1,3 @@
+class Restaurant <
+  has_many :reviews
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "0614490631",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "0614490640",
+    category:     "french"
+
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "0614490660",
+    category:     "french"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
